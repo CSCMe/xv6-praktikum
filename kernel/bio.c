0e321aab97d224a7b12e7f4cb06df673ef1f14c3
smartini <uhpfy@student.kit.edu>
2023-05-14 16:39:42 +0200

    UPD: WIP: buffer binary insert & size

--- kernel/bio.c
+++ kernel/bio.c
@@ -19,26 +19,23 @@
 
 
 
-typedef struct {
-  BigBuf* buffer;
-  union {
-    uint64 searchKey;
-    struct {
-      uint32 device;
-      uint32 blocknum;
-    };
-  };
-  uint64 lastReleased;
-} bsearchEntry;
-
-
 struct {
   struct spinlock lock;
   // Buffer are in array
   BigBuf cachedBuffers[NBUF];
   bsearchEntry bsearchEntries[NBUF];
-  uint64 accessCounter;
 } bcache;
+
+
+void debug_bsearch() {
+  pr_debug("PRINTING: BSEARCH--------------------------------------\n");
+  for (int i = 0 ; i < NBUF; i++) {
+    bsearchEntry* entry = bcache.bsearchEntries + i;
+    pr_debug("ind:%d, sK:%p, size: %d\n",i,entry->searchKey, entry->buffer->size);
+  }
+  pr_debug("END-----------------------------------------\n");
+}
+
 
 void
 binit(void)
@@ -46,11 +43,10 @@ binit(void)
   BigBuf *b;
 
   initlock(&bcache.lock, "bcache");
-  bcache.accessCounter = 0;
 
   for (int i = 0 ; i < NBUF; i++) {
     b = bcache.cachedBuffers + i;
-    
+    b->size = 0;
     b->page = kalloc();
     if (b->page == 0) {
       panic("binit: kalloc buffer alloc fail");
@@ -59,41 +55,107 @@ binit(void)
     entry->device = -1;
     entry->blocknum = 0;
     entry->buffer = b;
+    pr_debug("Invalid start key:%p\n", entry->searchKey);
   }
 }
 
 static int binary_search(int start, int end, uint64 targetKey) {
-  int mid = (start + end) / 2;
-  bsearchEntry* midEntry = bcache.bsearchEntries + mid;
-  if (start == end) {
-    return -1;
-  } else if (targetKey < midEntry->searchKey + BLOCKS_PER_PAGE && targetKey >= midEntry->searchKey){
-    return mid;
-  } else if (targetKey < midEntry->searchKey) {
-    return binary_search(start, mid, targetKey);
-  } else if (targetKey > midEntry->searchKey) {
-    return binary_search(mid + 1, end, targetKey);
+
+  
+  while (start != end) {
+    int mid = (start + end) / 2;
+    //pr_debug("start_%d, end:%d\n", start, end);
+    bsearchEntry* midEntry = bcache.bsearchEntries + mid;
+
+    // Found correct entry
+    if (targetKey < (midEntry->searchKey + midEntry->buffer->size) && targetKey >= midEntry->searchKey) {
+
+      return mid;
+    } else if (targetKey < midEntry->searchKey) {
+      end = mid;
+      continue;
+    } else if(targetKey > midEntry->searchKey) {
+      if (start == mid) {
+        //pr_debug("oh no\n");
+        return -1;
+      }
+      start = mid;
+      continue;
+    }
   }
+  // Haven't found fit
+  return -1;
 }
 
 /**
- * TODO: REDO, cuz pointers are not static or smth
+ * Assumption: List is sorted except for element at index index
+ * Sorts entry at index index into its correct location
+ * Return the new location
 */
-void binary_insert(int index) 
+int binary_insert(int index) 
 {
-  bsearchEntry* searchEntry = bcache.bsearchEntries + index;
-  for (int i = 0; i < NBUF; i++) {
+  bsearchEntry searchEntry = *(bcache.bsearchEntries + index);
+  
+  //pr_debug("ind:%d, sK:%p, lR:%p\n",index,searchEntry.searchKey,searchEntry.lastReleased);
+
+  int newLoc = NBUF - 1;
+  for (int i = 0; i < NBUF; i++) { // To look for new location
     bsearchEntry* curEntry = (bcache.bsearchEntries + i);
-    if (i == index) {
+    if (index == i) {
       continue;
     }
-    if (searchEntry)
+    if (searchEntry.searchKey == curEntry->searchKey) {
+      panic("buffer: binary_insert element already present");
+    }
+    if (searchEntry.searchKey < curEntry->searchKey) {
+      newLoc = i;
+      break;
+    }
   }
+/* 
+  pr_debug("newLoc: %d, old:%d\n", newLoc, index);
+  pr_debug("BFORE:REORDER: ");
+  debug_bsearch(); */
+  
+  // We need to move things to the left 
+  if (index < newLoc) {
+    newLoc = newLoc - 1;
+    for (int i = index; i < newLoc  && i < (NBUF - 1); i++) { 
+      bsearchEntry copy = bcache.bsearchEntries[i];
+      bcache.bsearchEntries[i] = bcache.bsearchEntries[i + 1];
+      bcache.bsearchEntries[i + 1] = copy;
+    }
+  } else {
+    // We need to move everything to the right 
+    for (int i = index; i > newLoc && i > 0; i--) { 
+      bsearchEntry copy = bcache.bsearchEntries[i];
+      bcache.bsearchEntries[i] = bcache.bsearchEntries[i - 1];
+      bcache.bsearchEntries[i - 1] = copy;
+    }
+  }
+
+
+
+  return newLoc;
+/*   pr_debug("END:  ");
+  debug_bsearch(); */
 }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
+
+void updateSize(bsearchEntry* updated, bsearchEntry* after) {
+    uint64 newSize = ((after->searchKey - updated->searchKey));
+      if (newPos < NBUF - 1 && newSize < BLOCKS_PER_PAGE) {
+        big->size = newSize;
+        if (newSize == 0)
+          panic("buffer: Limited size to 0");  
+        //debug_bsearch();
+      } else {
+        big->size = BLOCKS_PER_PAGE;
+      }
+}
 static struct buf*
 bget(uint dev, uint blockno)
 {
@@ -101,10 +163,11 @@ bget(uint dev, uint blockno)
 
   acquire(&bcache.lock);
 
-  int searchResult = binary_search(0, NBUF, (((uint64)dev) << 32 ) + blockno);
+  int searchResult = binary_search(0, NBUF, ((uint64) dev << 32) + blockno);
 
   // Entry is cached
   if (searchResult != -1) {
+    big = bcache.bsearchEntries[searchResult].buffer;
     big->refcount++;
     int index = blockno - big->blockno;
     release(&bcache.lock);
@@ -113,13 +176,24 @@ bget(uint dev, uint blockno)
 
   // Entry is not cached, first possible
   for (int i = 0; i < NBUF; i++) {
-    big = (bcache.bsearchEntries + i )->buffer;
+    bsearchEntry* entry = (bcache.bsearchEntries + i );
+    big = entry->buffer;
     if (big->refcount == 0) {
       big->device = dev;
       big->blockno = blockno;
       big->refcount = 1;
+      entry->blocknum = blockno;
+      entry->device = dev;
 
-      binary_insert(i);
+      // Adjust old entry prev size
+      if (i != 0) {
+        (entry - 1)->buffer->size = (entry+1)->
+      }
+
+      int newPos = binary_insert(i);
+      entry = bcache.bsearchEntries + newPos;
+      debug_bsearch();
+
       
       struct buf* b = &(big->smallBuf[0]);
       for (int i = 0; i < BLOCKS_PER_PAGE; i++) {
@@ -128,7 +202,6 @@ bget(uint dev, uint blockno)
         (b + i)->data = big->page + (i * BSIZE);
         (b + i)->parent = big;
       }
-
       release(&bcache.lock);
       return b;
     }
--- kernel/buf.h
+++ kernel/buf.h
@@ -26,10 +26,22 @@ typedef struct __BigBuf {
   int disk;       // does disk "own" buf?
   uint device;    // device the data is from
   uint blockno;   // starting blockno, aligned to BUFFER_SIZE
-  uchar* page;    // Page
   uint32 refcount; // num of references to PageBuffer 
+  uint32 size;    // (1-BLOCKS_PER_PAGE) Size of the bigbuf, how many smallBufs are usable?
   struct buf smallBuf[BLOCKS_PER_PAGE];
+  uchar* page;    // Page
 } BigBuf;
+
+typedef struct {
+  BigBuf* buffer;
+  union {
+    uint64 searchKey;
+    struct {
+      uint32 blocknum;
+      uint32 device;
+    };
+  };
+} bsearchEntry;
 
 
 #ifdef __cplusplus
