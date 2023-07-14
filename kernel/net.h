8760e9fdaa5ee3e4cad93b696070a2983195fa55
smartini <uhpfy@student.kit.edu>
2023-07-14 11:52:07 +0200

    ADD: Tets IPv4 packet sending

--- kernel/net.h
+++ kernel/net.h
@@ -34,9 +34,9 @@ struct ethernet_header {
     uint8 src[MAC_ADDR_SIZE];
     // Len/Type field sent with the packet
     union {
-        // Length of Ethernet frame. Valid values 0-1500   CONVERTED TO LITTLE ENDIAN
+        // Length of Ethernet frame. Valid values 0-1500   CONVERTED TO BIG ENDIAN
         uint16 len;
-        // Type of Ethernet frame. Valid values 1536-65535 CONVERTED TO LITTLE ENDIAN
+        // Type of Ethernet frame. Valid values 1536-65535 CONVERTED TO BIG ENDIAN
         uint16 type;
     }; 
     uint8 data[]; // Data array of unspecified length
@@ -107,27 +107,89 @@ struct arp_packet {
 };
 
 /**
- * IPv4 header for an IPv4 packet
- * Without options/padding
+ * Defines for IPv4 headers
 */
+// Define for IPv4
+#define IP_VERSION_4             4
+// Not supported (for now) but still defined
+#define IP_VERSION_6             6 
 
+// Flag to not fragment packet
+#define IPv4_FLAG_DF  0x2
+// Indicates that more fragments come after this. Unset if last/only.
+#define IPv4_FLAG_MF  0x1
+
+#define IP_PROT_ICMP    0x01
+#define IP_PROT_TCP     0x06
+#define IP_PROT_UDP     0x11
+#define IP_PROT_TESTING 0xFD
+
+/**
+ * IPv4 header for an IPv4 packet
+ * No support for options/padding
+*/
 struct ipv4_header {
+    /**
+     * Bitfield for version and header length
+     * Swapped around because endian(?)
+    */
     struct {
-        uint8 version : 4;
+        /**
+         * Length of header
+         * Premultiplied by 32. Since we don't support options or padding it's always 5
+        */
         uint8 header_length : 4;
+        /**
+         * IP Protocol version
+         * Either IP_VERSION_4 or IP_VERSION_6
+        */
+        uint8 version : 4;
     };
+    // Complicated and congestion control. Just set to 0
     uint8 type_of_service;
+    // Total length of IP packet in bytes. Includes data & header. CONVERT TO BIG ENDIAN!
     uint16 total_length;
-    uint16 identified;
-    struct {
-        uint16 flags : 3;
-        uint16 fragment_offset : 13;
-    };
+    // Unique number for IP fragmentation and reassembly. Just set to 0
+    uint16 identification;
+    /**
+     * Union containing flags and fragment offset
+     * Watch endianness
+    */
+   union {
+        /**
+         * Bit field for fragment offset/flags field
+         * Flags and fragment offset swapped because endian
+        */
+        struct {
+
+            // What position a fragmented packet's data starts at when reassembled
+            uint16 fragment_offset : 13;
+            /**
+            * Bit of weirdness because endianness. Use Defines
+            * Bit 0: always 0
+            * Bit 1: Don't Fragment (IPv4_FLAGS_DF)
+            * Bit 2: More Fragments (IPv4_FLAGS_MF)
+            */
+            uint16 flags : 3;
+        };
+        // Needed to convert endian
+        uint16 flags_fragment_mixed;
+   };
+    // Describes lifetime of packet. De-facto max hop-count
     uint8 time_to_live;
+    /**
+     * ICMP: IP_PROT_ICMP
+     * TCP:  IP_PROT_TCP
+     * UDP:  IP_PROT_UDP
+    */
     uint8 protocol;
+    // Header checksum. 0 for sake of calculation. CONVERT TO BIG ENDIAN (?)
     uint16 header_checksum;
+    // Source IP-Addr
     uint8 src[IP_ADDR_SIZE];
+    // Dest IP-Addr
     uint8 dst[IP_ADDR_SIZE];
+    // Contained data
     uint8 data[];
 };
 
--- kernel/virtio_net.c
+++ kernel/virtio_net.c
@@ -170,13 +170,29 @@ send_ethernet_packet(uint8 dest_mac[MAC_ADDR_SIZE], enum EtherType type, void* d
 
   release(&net_card.net_lock);
 }
-/*
+
 void
 test_send_ip()
 {
   struct ipv4_header actual_ip_header = {0};
-  struct ipv4_headaer* ipv4_header = &actual_ip_header;
-}*/
+  struct ipv4_header* packet = &actual_ip_header;
+
+  uint8 ipdest[8] = {10, 0, 2, 3};
+  memmove((void*) packet->dst, ipdest, 4);
+  uint8 ipme[8] = {10, 0, 2, 15};
+  memmove((void*) packet->src, ipme, 4);
+  packet->total_length = 20;
+  memreverse(&packet->total_length, sizeof(packet->total_length));
+  packet->header_length = 5;
+  packet->protocol = IP_PROT_TESTING;
+  packet->time_to_live = 64;
+  packet->version = IP_VERSION_4;
+  packet->flags = 0;
+  memreverse(&packet->flags_fragment_mixed, 2);
+  //packet->fragment_offset = 4096;
+  uint8 dest[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+  send_ethernet_packet(dest, ETHERNET_TYPE_IPv4, (void*) packet, sizeof(struct ipv4_header));
+}
 
 void
 test_send_arp()
@@ -319,7 +335,7 @@ virtio_net_init(void)
   // Exposes receive buffer to card
   net_card.receive.driver->idx++;
 
-  test_send_arp();
+  test_send_ip();
 
 }
 
